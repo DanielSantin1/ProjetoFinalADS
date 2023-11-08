88dbd4ce372e6b647694f85c0a16756a1601843a
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2023-11-08 16:00:11 -0300

    FINALMENTE
    ACABOU

--- pizzaria/src/routes/app.routes.tsx
+++ pizzaria/src/routes/app.routes.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Dashboard from '../pages/Dashboard';
-import Order from '../pages/Order';
+import Order from '../pages/Order'
+import FinishOrder from '../pages/FinishOrder'
 
 export type StackPramsList = {
   Dashboard: undefined;
@@ -10,24 +11,39 @@ export type StackPramsList = {
     number: number | string;
     order_id: string;
   };
-}
-
+  FinishOrder: {
+    number: number | string;
+    order_id: string;
+  };
+};
 
 const Stack = createNativeStackNavigator<StackPramsList>();
 
-function AppRoutes() {
-  return (
+function AppRoutes(){
+  return(
     <Stack.Navigator>
-      <Stack.Screen
-        name="Dashboard"
-        component={Dashboard}
+      <Stack.Screen 
+        name="Dashboard" 
+        component={Dashboard} 
         options={{ headerShown: false }}
       />
-      <Stack.Screen
-      name="Order"
-      component={Order}
-      options={{headerShown: false}}
 
+      <Stack.Screen
+        name="Order"
+        component={Order}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="FinishOrder"
+        component={FinishOrder}
+        options={{
+          title: 'Finalizando',
+          headerStyle:{
+            backgroundColor: '#1d1d2e'
+          },
+          headerTintColor: '#FFF'
+        }}
       />
     </Stack.Navigator>
   )
